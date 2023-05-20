4352c6c7722df22a41f3b7de10b3ee92b7e3fed0
ujnvdprasad <124246569+ujnvdprasad@users.noreply.github.com>
2023-05-20 17:45:45 +0530

    Update App.java

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Devops Class" );
+        System.out.println( "This is Durga Prasad" );
     }
 }
